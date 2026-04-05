9b85168b68661351bf65ba77f195b5ccc436894e
jatinthelord <jatinthelord@gmail.com>
2026-04-05 12:35:36 +0530

    NHP.cmd

---
+++ NHP.cmd
@@ -0,0 +1,6 @@
+git init
+git add nhp_compiler.c
+git commit -m "NHP Language v1.0"
+# GitHub pe new repo banao, phir:
+git remote add origin https://github.com/jatinthelord/NHP
+git push -u origin main
